4d4bed6f313be0f188d5a0447f90772ff0222925
Evan H. Anders <evanhanders@gmail.com>
2018-07-24 13:52:36 -0600

    adds IC for b=-1 (tol=1e-7)

--- code/IC_finder.py
+++ code/IC_finder.py
@@ -14,29 +14,29 @@ bc_dict = {
 
 n_rho_cz=3
 
+increase_factor = -0.02
+decrease_factor = increase_factor*2
 
-nz = 512
-true_kram_b = -0.35
-kram_b = true_kram_b
+nz = 1024
+true_kram_b = -1
+kram_b = -0.5
 atmosphere = polytropes.FC_polytrope_2d_kramers(bc_dict, nz=nz, kram_b=kram_b, no_equil=True, dimensions=1, n_rho_cz=n_rho_cz)
 
 
-tol=1e-10
+tol=1e-7
 ncc_cutoff=np.abs(true_kram_b)*1e-6
 
-increase_factor = np.sqrt(2)
-decrease_factor = 2
 flux_factor = 1
 while(True):
     print('EQUILIBRATING AT KRAM B = {}'.format(kram_b))
     atmosphere._equilibrate_atmosphere(bc_dict, ncc_cutoff=ncc_cutoff, tolerance=tol)
     if np.isnan(np.max(atmosphere.T0['g'])):
-        kram_b /= decrease_factor
+        kram_b -= decrease_factor
         print('DECREASING KRAM B BY FACTOR OF {} TO {}'.format(decrease_factor, kram_b))
         atmosphere = polytropes.FC_polytrope_2d_kramers(bc_dict, nz=nz, kram_b=kram_b, no_equil=True, dimensions=1, n_rho_cz=n_rho_cz)
         flux_factor = 1
     elif kram_b > true_kram_b:
-        kram_b *= increase_factor
+        kram_b += increase_factor
         print('INCREASING KRAM B BY FACTOR OF root(2) TO {}'.format(kram_b))
         new_atmosphere = polytropes.FC_polytrope_2d_kramers(bc_dict, nz=nz, kram_b=kram_b, no_equil=True, dimensions=1, n_rho_cz=n_rho_cz)
         atmosphere.T0.set_scales(1, keep_data=True)
